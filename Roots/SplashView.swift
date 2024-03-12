a69a32953ac44c2b309b784205f91196c86f099d
Brody Wilton <brodywilton@hotmail.com>
2024-03-12 17:55:04 +0000

    Add splash page

---
+++ Roots/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  Roots
+//
+//  Created by Brody Wilton on 12/03/2024.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
